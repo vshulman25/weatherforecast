5ac6e397ebf5a2641c170c54fdcb8fd0e10ca508
Vickee Shulman Wojciechowski <vickeeshulmanwojciechowski@Vickees-MacBook-Air.local>
2021-01-15 22:25:10 -0500

    changes to js and html

--- script.js
+++ script.js
@@ -5,13 +5,19 @@ $(document).ready(function () {
   $(".search-btn").on("click", function () {
     let searchValue = $(".search-value").val()
 
+    // $(".history").on("click", "li", function () {
+    //   searchWeather($(this).text())
+    // })
 
+    // function makeRow(text) {
+    //   let li = $("<li>").addClass("list-group-history").text(text)
+    //   $(history).append(li)
+    // }
 
-
-
-    console.log(searchValue)
+    // console.log(searchValue)
     mainWeatherSearch(searchValue)
-    forescastSearch(searchValue)
+    forecastSearch(searchValue)
+    // makeRow(searchValue)
   })
 
 
@@ -20,10 +26,17 @@ $(document).ready(function () {
   function mainWeatherSearch(searchValue) {
     let APIkey = "931abc2f197b56ef2fd113e5d91582df";
     let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchValue + "&appid=" + APIkey;
-
     $.ajax({
       url: queryURL,
       method: "GET"
+      // }).then(function (data) {
+      //   if (history.indexOf(searchValue)=== -1) {
+      //     history.push.indexOf(searchValue)
+      //     window.localStorage.setItem("history", JSON.stringify(history))
+
+      //     makeRow(searchValue)
+      //   }
+
     }).then(function (response) {
       console.log(response)
       let cityname = $("<h1>").text(response.name)
@@ -32,15 +45,18 @@ $(document).ready(function () {
       let wind = $("<div>").text("Wind Speed: " + response.wind.speed)
       let humidity = $("<div>").text("Humidity: " + response.main.humidity)
       let img = $("<img>").attr("src", " http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
-
+      // let todayDate = response.list[0].dt_txt;
 
       $("#today").append(cityname, temp, wind, humidity)
       $("#today").append(img)
     });
   }
 
+
+
+
   // this function shows the 5 day forecast for the location inputted //
-  function forescastSearch(searchValue) {
+  function forecastSearch(searchValue) {
     let APIkey = "931abc2f197b56ef2fd113e5d91582df";
     let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIkey;
 
@@ -49,40 +65,19 @@ $(document).ready(function () {
       method: "GET"
     }).then(function (response) {
       console.log(response)
-      $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\">");
+      $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\" + \"row\">");
       for (let i = 0; i < response.list.length; i++) {
         // only look at forecasts around 12:00pm - card for each day when it hits 12 x 5 
         if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
           // create html elements for a bootstrap card
           let forecastData = ((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2)
           let forecastTemp = $("<ol>").text(forecastData)
-          $("#forecast .row").append(forecastTemp)
+          let forecastHumData = ((response.list[i].main.humidity)).toFixed(2)
+          let forecastHum = $("<br> <ol>").text(forecastHumData)
+          $("#forecast .row").append(forecastTemp, forecastHum)
         }
       }
     });
   }
 
-  // this will call the UV index API 
-
-
-
-
-  })
-
-
-
-      // function for uv + var for lon and lat and 5 day + var info for others outside of city and date, append those 
-
-
-
-
-
-
-
-
-
-
-
-
-
-// ------
+})
