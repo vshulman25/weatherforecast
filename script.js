dc5a703150ebd8708ebdd9be8aca10908a5f2c6c
Vickee Shulman Wojciechowski <vickeeshulmanwojciechowski@Vickees-MacBook-Air.local>
2021-01-16 16:53:03 -0500

    changes to index and js to pull in uv

--- script.js
+++ script.js
@@ -47,37 +47,48 @@ $(document).ready(function () {
       let img = $("<img>").attr("src", " http://openweathermap.org/img/w/" + response.weather[0].icon + ".png")
       // let todayDate = response.list[0].dt_txt;
 
-      $("#today").append(cityname, temp, wind, humidity)
-      $("#today").append(img)
-    });
+      let APIkey = "931abc2f197b56ef2fd113e5d91582df";
+      let lat = $()
+      let queryURL = "http://api.openweathermap.org/data/2.5/uvi?lat=" + "&lon=" + "&appid=" + APIkey;
+      $.ajax({
+        url: queryURL,
+        method: "GET"
+      })
+
+
+
+    $("#today").append(cityname, temp, wind, humidity)
+    $("#today").append(img)
+  });
   }
 
 
 
 
-  // this function shows the 5 day forecast for the location inputted //
-  function forecastSearch(searchValue) {
-    let APIkey = "931abc2f197b56ef2fd113e5d91582df";
-    let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIkey;
+// this function shows the 5 day forecast for the location inputted //
+function forecastSearch(searchValue) {
+  let APIkey = "931abc2f197b56ef2fd113e5d91582df";
+  let queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + searchValue + "&appid=" + APIkey;
 
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function (response) {
-      console.log(response)
-      $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\" + \"row\">");
-      for (let i = 0; i < response.list.length; i++) {
-        // only look at forecasts around 12:00pm - card for each day when it hits 12 x 5 
-        if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
-          // create html elements for a bootstrap card
-          let forecastData = ((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2)
-          let forecastTemp = $("<ol>").text(forecastData)
-          let forecastHumData = ((response.list[i].main.humidity)).toFixed(2)
-          let forecastHum = $("<br> <ol>").text(forecastHumData)
-          $("#forecast .row").append(forecastTemp, forecastHum)
-        }
+  $.ajax({
+    url: queryURL,
+    method: "GET"
+  }).then(function (response) {
+    console.log(response)
+    $("#forecast").html("<h4 class=\"mt-3\">5-Day Forecast:</h4>").append("<div class=\"row\" + \"row\">");
+    for (let i = 0; i < response.list.length; i++) {
+      // only look at forecasts around 12:00pm - card for each day when it hits 12 x 5 
+      if (response.list[i].dt_txt.indexOf("12:00:00") !== -1) {
+        // create html elements for a bootstrap card
+        let forecastData = ((response.list[i].main.temp - 273.15) * 1.80 + 32).toFixed(2)
+        let forecastTemp = $("<ol>").text(forecastData)
+        let forecastHumData = ((response.list[i].main.humidity)).toFixed(2)
+        let forecastHum = $("<br> <ol>").text(forecastHumData)
+        $("#forecast .row").append(forecastTemp, forecastHum)
       }
-    });
-  }
+    }
+  });
+}
 
 })
+
